7742cdcf3afdcd5821180848ff1ba37eaf41929b
illika <steve.milan7@gmail.com>
2023-01-13 23:16:17 -0500

    Componentes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function saludo() {
+  return <h1>Hola mundo - Funcion Saludo</h1>;
+}
+
+const Componente2 = () => {
+  return <h2>Hola mundo - Componente2 </h2>;
+}
+
+root.render(<div>
+  {saludo()}
+  <Componente2></Componente2>
+</div>);
