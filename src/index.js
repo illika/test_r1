573dfe80d1fc5673a77bbc6c048f511a2e463cd8
illika <steve.milan7@gmail.com>
2023-01-14 22:15:52 -0500

    useEffect

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 function Counter() {
 
+  const [mensaje, setMensaje] = useState('');
   const [contador, setContador] = useState(0);
 
+  useEffect(() => {
+    console.log("render");
+  }, [contador])
+
   return <div>
+    <input onChange={(e)=> {
+      setMensaje(e.target.value);
+    }}></input>
+    <button onClick={()=>{
+      alert("el mensaje es " + mensaje);
+    }}>Alerta</button>
+    <hr></hr>
     <h1>Counter: {contador}</h1>
     <button onClick={() => {
       setContador(contador + 1)
