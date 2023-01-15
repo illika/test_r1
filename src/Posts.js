da5c4d5410bf26b4edb4f56ca47bf75d39d8cd85
illika <steve.milan7@gmail.com>
2023-01-14 21:37:26 -0500

    map | key

--- src/Posts.js
+++ src/Posts.js
@@ -2,7 +2,7 @@ import {VscGlobe} from 'react-icons/vsc';
 
 export const Posts = () => {
     return <button onClick={(e) => {
-        fetch('https://jsonplaceholder11.typicode.com/posts')
+        fetch('https://jsonplaceholder.typicode.com/posts')
         .then(response => response.json())
         .then(data => console.log(data))
         .catch(error => console.log(error))
--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { Saludo, UserCard } from './Saludo';
-import Product, { Campo } from './Product';
-import {Button} from './Button';
-import {TaskCard} from './Task';
-
-import { Welcome } from './Welcome';
-
 import { Posts } from './Posts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const Componente2 = () => {
-  return <h2>Hola mundo - Componente2 </h2>;
-}
-
-const handleChange = (e) => {
-  console.log(e.target.value)
-}
+const user = [
+  {
+    id: 1,
+    name: 'John',
+    image: 'https://robohash.org/user1'
+  },
+  {
+    id: 2,
+    name: 'Juan',
+    image: 'https://robohash.org/user2'
+  }
+]
 
 root.render(<>
-  <Posts></Posts>
+  {user.map((item, index) => {
+    return <div key={index}>
+      <p>{item.id} - {item.name}</p>
+      <img src={item.image}></img>
+    </div>
+  })}
 </>);
